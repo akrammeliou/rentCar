e8606120c9df88e86026381aeb589fa009e244e8
akrammeliou <melioua23@gmail.com>
2019-11-26 20:20:34 +0100

    akramm

--- dist/scripts/userJS/user_profile.js
+++ dist/scripts/userJS/user_profile.js
@@ -8,6 +8,8 @@ function profile() {
         document.getElementById('user_profile').innerHTML = htmlprofile
 }
 
+/********* user profile ***********/
+
 let user = JSON.parse(localStorage.getItem("connectedUser"));
 
 let tdUsername = document.createElement("td");
@@ -26,3 +28,63 @@ let tr = document.getElementById("trtable");
 tr.appendChild(tdUsername);
 tr.appendChild(tdEmail);
 tr.appendChild(tdPassword);
+
+/********* Consult cars user profile ***********/
+var cars = JSON.parse(localStorage.getItem('reservation')) || [];
+var connectedUser = JSON.parse(localStorage.getItem("connectedUser")) || [];
+
+
+cars.forEach(reservation => {
+    if(connectedUser.id == reservation.owner) {
+        console.log(reservation.owner);
+        console.log(connectedUser.id);
+        
+    let tr = document.createElement('tr');
+    
+    let tdidvoiture = document.createElement("td");
+    let tdResDebut = document.createElement("td");
+    let tdResFin = document.createElement("td");
+
+    let textidvoiture = document.createTextNode(reservation.idvoiture);
+    let textResDebut = document.createTextNode(reservation.dateresrvationdebut);
+    let textResFin = document.createTextNode(reservation.dateresrvationfin);
+    
+    tdidvoiture.appendChild(textidvoiture);
+    tdResDebut.appendChild(textResDebut);
+    tdResFin.appendChild(textResFin);
+    
+    tr.appendChild(textidvoiture);
+    tr.appendChild(tdResDebut);
+    tr.appendChild(tdResFin);
+    
+    let body = document.getElementById("bodytable");
+    body.appendChild(tr);    
+}
+})
+;
+
+/*********** List of users profile *************/
+
+// let users = JSON.parse(localStorage.getItem("user"));
+// users.forEach(user => {
+//     let tr = document.createElement('tr');
+    
+//     let tdUsername = document.createElement("td");
+//     let tdEmail = document.createElement("td");
+//     let tdPassword = document.createElement("td");
+    
+//     let textUsername = document.createTextNode(user.username);
+//     let textEmail = document.createTextNode(user.pass);
+//     let textPassword = document.createTextNode(user.email);
+    
+//     tdUsername.appendChild(textUsername);
+//     tdEmail.appendChild(textEmail);
+//     tdPassword.appendChild(textPassword);
+    
+//     tr.appendChild(tdUsername);
+//     tr.appendChild(tdEmail);
+//     tr.appendChild(tdPassword);
+    
+//     let body = document.getElementById("bodytable");
+//     body.appendChild(tr);    
+// });
