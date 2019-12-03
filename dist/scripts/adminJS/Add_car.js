b7b8e51efefa7aec8adf33aa2f63d7a898b94968
akrammeliou <melioua23@gmail.com>
2019-12-03 20:01:44 +0100

    testtakram

--- dist/scripts/adminJS/Add_car.js
+++ dist/scripts/adminJS/Add_car.js
@@ -41,7 +41,7 @@ function affichermodele() {
   }
   modele += `</select>`;
   document.getElementById('modele').innerHTML = modele
-  
+
 
 }
 
@@ -63,8 +63,8 @@ function insertvoiture() {
     matricule: document.getElementById('matricule').value,
     owner: connectedUser.id,
     modele: modelle,
-    marquevoiture:id,
-    image : document.getElementById('carImg').files[0].name,
+    marquevoiture: id,
+    image: document.getElementById('carImg').files[0].name,
     id: Math.floor(Math.random() * 1000 + 1),
   }
   tabvoiture.push(voituer);
@@ -74,13 +74,14 @@ function insertvoiture() {
 }
 
 function affichagevoitureAdmin() {
-  let htmladmin=`<h1>`;
+  let htmladmin = `<h1>`;
   let html = ` <table class="table table-middle">
   <thead>
     <tr>
-  <th>Matricule</th>
+      <th>Matricule</th>
       <th>Marque</th>
       <th>Modele</th>
+      <th>Photo</th>
       <th>Progress</th>
     </tr>
   </thead> <tbody>`
@@ -90,69 +91,65 @@ function affichagevoitureAdmin() {
   var connectedUser = JSON.parse(localStorage.getItem("connectedUser")) || [];
   for (let i = 0; i < tab.length; i++) {
     if (tab[i].id == connectedUser.id) {
-      
-      htmladmin+=`<h5>Les voiture de l'admin est ${connectedUser.username}</h5>`;
+
+      htmladmin += `<h5>Les voiture de l'admin est ${connectedUser.username}</h5>`;
       for (let j = 0; j < tabvoiture.length; j++) {
         console.log('bach voiture ' + tabvoiture[j].marquevoiture);
-        html += 
-            `<tr>
-        <td id="name"> 
-        ${tabvoiture[j].matricule}              
-        </td>
-        <td id="marque">
-        <img src='../../assets/images/${tabvoiture[j].image}' class="img-thumb-sm mr-3">
-        ${tabvoiture[j].marquevoiture}
-          </td>
-          <td>${tabvoiture[j].modele}</td>
-        <td style="">
-          <div class="py-1 px-1">
-            <button class="btn btn-sm btn-flat-info btn-fab id="edit" onclick="ediiiit(${tabvoiture[j].id})" ><i class="material-icons">edit</i></button>
-            <button class="btn btn-sm btn-flat-secondary btn-fab id="delete" onclick="delete_cars(${tabvoiture[j].id})" ><i class="material-icons">delete</i></button>
-          </div>
-        </td>
-      </tr>`
+        html +=
+          `<tr>
+            <td id="name">${tabvoiture[j].matricule}</td>
+            <td>${tabvoiture[j].marquevoiture}</td>
+            <td>${tabvoiture[j].modele}</td>
+            <td id="marque">
+              <img src='../../assets/images/${tabvoiture[j].image}' class="img-thumb-sm mr-3">
+            </td>
+            <td style="">
+              <div class="py-1 px-1">
+                <button class="btn btn-sm btn-flat-info btn-fab id="edit" onclick="ediiiit(${tabvoiture[j].id})" ><i class="material-icons">edit</i></button>
+                <button class="btn btn-sm btn-flat-secondary btn-fab id="delete" onclick="delete_cars(${tabvoiture[j].id})" ><i class="material-icons">delete</i></button>
+              </div>
+            </td>
+          </tr>`
       }
     }
   }
-  htmladmin+=`</h1>`
-  html+=`</tbody> </table>`;
+  htmladmin += `</h1>`
+  html += `</tbody> </table>`;
   document.getElementById('tabvoiture').innerHTML = html
-  document.getElementById('admintitle').innerHTML=htmladmin;
+  document.getElementById('admintitle').innerHTML = htmladmin;
 }
 function delete_cars(id) {
   console.log(id);
-  
+
   var tabvoiture = JSON.parse(localStorage.getItem('voituer')) || [];
-    for (let j = 0; j < tabvoiture.length; j++) {
-      if (tabvoiture[j].id === id) {
-        tabvoiture.splice(j, 1);
-      }
+  for (let j = 0; j < tabvoiture.length; j++) {
+    if (tabvoiture[j].id === id) {
+      tabvoiture.splice(j, 1);
     }
+  }
   localStorage.setItem("voituer", JSON.stringify(tabvoiture));
   affichagevoitureAdmin()
 }
 
 function ediiiit(id) {
   var tabvoiture = JSON.parse(localStorage.getItem('voituer')) || [];
-  let index 
-    for (let i = 0; i < tabvoiture.length; i++) {
-        if (id == tabvoiture[i].id) {
-          index = i
-        }
+  let index
+  for (let i = 0; i < tabvoiture.length; i++) {
+    if (id == tabvoiture[i].id) {
+      index = i
     }
-  let htmledit =`<table id="edittt">
+  }
+  let htmledit = `<table id="edittt">
       <tr>
-        <td><input type="texte" class="form-control" id="matricule_edit" value="${tabvoiture[index].matricule}"></td>
-        <td><input type="texte" class="form-control" id="mark_edit" value="${tabvoiture[index].marquevoiture}"> 
-         <td><input type="texte" class="form-control" id="modele_edit" value="${tabvoiture[index].modele}"></td>
-         <td>
-         <label class="btn btn-outline-primary"> Upload
-          <input id='carImg'type="file" value="../../assets/images/${tabvoiture[index].image}" accept="image/*" onchange="preview_image(event)" class="uploadFile img" value="Upload Photo"
-               style="width: 0px;height: 0px;overflow: hidden;"></label>
-          <img src='../../assets/images/${tabvoiture[index].image}' id="image_edit" class="img-thumb-sm mr-3"></td>
         <td>
-          <input type="button" class="btn btn-primary" value="Edit" id="ediiit" onclick="modif(${tabvoiture[index].id})">
+          <label class="btn btn-outline-primary"> Upload
+            <input id='carImg' type="file" accept="image/*" onchange="preview_image(event)" class="uploadFile img" value="Upload Photo" style="width: 0px;height: 0px;overflow: hidden;">
+          </label>
         </td>
+        <td><input type="texte" class="form-control" id="matricule_edit" value="${tabvoiture[index].matricule}"></td>
+        <td><input type="texte" class="form-control" id="mark_edit" value="${tabvoiture[index].marquevoiture}"></td>
+        <td><input type="texte" class="form-control" id="modele_edit" value="${tabvoiture[index].modele}"></td>
+        <td><input type="button" class="btn btn-primary" value="Edit" id="ediiit" onclick="modif(${tabvoiture[index].id})"></td>
       </tr>
   </table>`
   document.getElementById('editform').innerHTML = htmledit
@@ -162,18 +159,18 @@ function modif(id) {
   var tabvoiture = JSON.parse(localStorage.getItem('voituer')) || [];
   var connectedUser = JSON.parse(localStorage.getItem("connectedUser")) || [];
   todo = {
-      id: id,
-      owner: JSON.parse(localStorage.getItem('connectedUser')).id,
-      matricule: document.getElementById('matricule_edit').value,
-      marquevoiture: document.getElementById('mark_edit').value,
-      modele: document.getElementById('modele_edit').value,
-      image:document.getElementById('image_edit').files[0].name
-      // image: document.getElementById('image_edit')
+    id: id,
+    owner: JSON.parse(localStorage.getItem('connectedUser')).id,
+    matricule: document.getElementById('matricule_edit').value,
+    marquevoiture: document.getElementById('mark_edit').value,
+    modele: document.getElementById('modele_edit').value,
+    image: document.getElementById('carImg').files[0].name
+    // image: document.getElementById('image_edit')
   }
   for (let i = 0; i < tabvoiture.length; i++) {
-      if (tabvoiture[i].id === id) {
-        tabvoiture.splice(i, 1, todo);
-      }
+    if (tabvoiture[i].id === id) {
+      tabvoiture.splice(i, 1, todo);
+    }
   }
   localStorage.setItem("voituer", JSON.stringify(tabvoiture));
   affichagevoitureAdmin();
@@ -183,13 +180,12 @@ function load() {
   window.location.reload()
 }
 
-function preview_image(event) 
-{
- var reader = new FileReader();
- reader.onload = function()
- {
-  var output = document.getElementById('output_image');
-  output.src = reader.result;
- }
- reader.readAsDataURL(event.target.files[0]);
+function preview_image(event) {
+  var reader = new FileReader();
+  reader.onload = function () {
+    var output = document.getElementById('output_image');
+    output.src = reader.result;
+  }
+  reader.readAsDataURL(event.target.files[0]);
 }
+// <!-- <img src='../../assets/images/${tabvoiture[index].image}' id="image_edit" class="img-thumb-sm mr-3"></td> -->
